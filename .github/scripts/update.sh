8c82df0b862fbc9a2a61f90a60d3f5b34d6241ac
Shivam Mathur <shivam_jpr@hotmail.com>
2025-05-16 01:07:25 +0000

    Fix update.sh [skip ci]

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,7 +1,7 @@
 
 LATEST_LTS_VERSION=$(curl -sL https://nodejs.org/download/release/index.json | jq -r '[.[] | select(.lts != false)] | first | .version | sub("^v"; "")')
 for file in Dockerfile*; do
-    sed -i "s/ENV NODE_VERSION .*/ENV NODE_VERSION $LATEST_LTS_VERSION/g" "$file"
+    sed -i "s/ENV NODE_VERSION=.*/ENV NODE_VERSION=$LATEST_LTS_VERSION/g" "$file"
 done
 sed -i "s/Node.js [0-9]*\.[0-9]*\.[0-9]*/Node.js $LATEST_LTS_VERSION/g" README.md
 echo "version=$LATEST_LTS_VERSION" >> $GITHUB_OUTPUT
